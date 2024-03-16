ade873adf0c00208fe6cc4efd7b7b7a632308230
ann2303 <anna.ilina.2001@yandex.ru>
2024-03-16 18:08:59 +0400

    [Homework 5] Add solution for task in 05_DiffPatchNet

---
+++ 05_DiffPatchNet/main.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+import asyncio
+import cowsay
+
+all_cows = set(cowsay.list_cows())
+clients = {}
+
+async def chat(reader, writer):
+    queue = asyncio.Queue()
+    is_registered = False
+    me = None
+    command_parse = asyncio.create_task(reader.readline())
+    receive = asyncio.create_task(queue.get())
+    while not reader.at_eof():
+        done, pending = await asyncio.wait([command_parse, receive], return_when=asyncio.FIRST_COMPLETED)
+        for q in done:
+            if q is command_parse:
+                command_parse = asyncio.create_task(reader.readline())
+                command = q.result().decode().strip().split()
+                if len(command) > 1:
+                    args = command[1:]
+                command = command[0]
+                if command == "yield":
+                    if not is_registered:
+                        writer.write("You need to login.\n".encode())
+                        await writer.drain()
+                    else:
+                        if len(args) == 1:
+                            for out in clients.values():
+                                if out is not clients[me]:
+                                    await out.put(f"{cowsay.cowsay(args[0], cow=me)}")
+                            await writer.drain()
+                elif command == "who":
+                    registered_cows = clients.keys()
+                    cows_str = f"Registered cows: {' '.join(registered_cows)}\n"
+                    writer.write(cows_str.encode())
+                    await writer.drain()
+                elif command == "cows":
+                    cows = set.difference(all_cows, clients.keys())
+                    cows_str = f"Free cows: {' '.join(cows)}\n"
+                    writer.write(cows_str.encode())
+                    await writer.drain()
+                elif command == "login":
+                    if is_registered:
+                        writer.write(f"You are registered.\n".encode())
+                        await writer.drain()
+                    else:
+                        me = args[0]
+                        if me not in all_cows:
+                            writer.write(f"The cow {me} doesn't exist.\n".encode())
+                            await writer.drain()
+                        elif me in clients:
+                            writer.write(f"The cow {me} is occupied by another user.\n".encode())
+                            await writer.drain()
+                        else:
+                            clients[me] = queue
+                            is_registered = True
+                elif command == "say":
+                    if not is_registered:
+                        writer.write("You need to login.\n".encode())
+                        await writer.drain()
+                    elif len(args) != 2:
+                        writer.write("You need to pass message and receiver.\n".encode())
+                        await writer.drain()
+                    else:
+                        reciever, message = args[0], args[1]
+                        if reciever in clients:
+                            await clients[reciever].put(f"{cowsay.cowsay(message, cow=me)}\n")
+                            await writer.drain()
+                        else:
+                            writer.write(f"{reciever} is not registered.\n".encode())
+                            await writer.drain()
+                elif command == 'quit':
+                    command_parse.cancel()
+                    receive.cancel()
+                    print(me, "DONE")
+                    del clients[me]
+                    writer.close()
+                    await writer.wait_closed()
+                    return
+                else: 
+                    writer.write("Incorrect command.\n".encode())
+                    await writer.drain()
+                    
+                        
+                    
+            elif q is receive:
+                receive = asyncio.create_task(clients[me].get())
+                writer.write(f"{q.result()}\n".encode())
+                await writer.drain()
+
+
+async def main():
+    server = await asyncio.start_server(chat, '0.0.0.0', 1337)
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
